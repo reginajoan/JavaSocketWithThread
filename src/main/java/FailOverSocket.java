71375f3d33f2917f776badd98735e6080c3fbab2
reginajoan <najoanregi@gmail.com>
2021-05-21 13:48:04 +0700

    update backend v1.1

--- src/main/java/FailOverSocket.java
+++ src/main/java/FailOverSocket.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 
 public class FailOverSocket{
     public static void main(String[] args) throws Exception {
-//        Server server = new Server();
-//        server.start();
-        String dataDB = "0957ATMDPRLOAN60110220002004844602211520018992  20200615103547000133005400000133  NGTLNAP6064201240152425    7303AAA13481D50C                                                                                                                                                                                                                                                                                                                                                                        20200615103547                                                                                                                                                                                                                                                                                                                                                                                                                                                             -SendAdToHLI\"";
-        RunningPrograms run = new RunningPrograms();
-        run.RunningProgram1(dataDB, 78,"172.16.1.33",9000);
+        Server server = new Server();
+        server.start();
+//        String dataDB = "0957ATMDPRLOAN60110220002004844602211520018992  20200615103547000133005400000133  NGTLNAP6064201240152425    7303AAA13481D50C                                                                                                                                                                                                                                                                                                                                                                        20200615103547                                                                                                                                                                                                                                                                                                                                                                                                                                                             -SendAdToHLI\"";
+//        RunningPrograms run = new RunningPrograms();
+//        run.RunningProgram1(dataDB, 78,"172.16.1.33",9000);
     }
 }
